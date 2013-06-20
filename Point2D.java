7ca90ce742254ed3f1fb96e52e2ea1284ec674de
Catherine <glenn@glenn-laptop.(none)>
2013-06-20 15:37:53 -0400

    Changed the name of the Point2D file.

---
+++ Point2D.java
@@ -0,0 +1,60 @@
+
+/**
+ *
+ * Point in a 2D coordinate plane.
+ *
+ * @author Catherine
+ *
+ */
+public class Point2D {
+
+	private double x;
+	private double y;
+
+	public Point2D( int x, int y ) 
+		setX( x );
+		setY( y );
+	}
+
+	public Point2D( float x, float y ) {
+		setX( x );
+		setY( y );
+	}
+
+	public Point2D( double x, double y ) {
+		setX( x );
+		setY( y );
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setX( int x ) {
+		this.x = ( double )x;
+	}
+
+	public void setX( float x ) {
+		this.x = ( double )x;
+	}
+
+	public void setX( double x ) {
+		this.x = x;
+	}
+
+	public void setY( int y ) {
+		this.y = ( double )y;
+	}
+
+	public void setY( float y ) {
+		this.y = ( double )y;
+	}
+
+	public void setY( double y ) 
+		this.y = y;
+	}
+}
